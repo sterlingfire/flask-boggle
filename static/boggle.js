0190a63c527cdacd039df76e9d3b51be122e5e85
J. Sterling Bishop <jsterlingbishop@gmail.com>
2020-12-04 16:03:39 -0800

    Implemented Displayboard

--- static/boggle.js
+++ static/boggle.js
@@ -21,10 +21,39 @@ async function start() {
 
 /** Display board */
 
-function displayBoard(board) {
-  // $table.empty();
+function displayBoard(dataBoard) {
+  $table.empty();
   // loop over board and create the DOM tr/td structure
+  let $tbody = $("<tbody>");
+  for (let dataRow of dataBoard){
+    let $tr = $("<tr>");
+    for(let dataLetter of dataRow){
+      let $td = $("<td>").text(dataLetter);
+      $tr.append($td);
+    }
+    $tbody.append($tr);
+  }
+  $table.append($tbody);
 }
 
+/** Display message */
 
+function displayMessage(message){
+  $message.text(message)
+}
+/** Check to see word is valid in the game board.
+ *  Sends API request to /api/score-word
+ */
+
+async function checkWord(evt) {
+  evt.preventDefault();
+  let word = $wordInput.val();
+  let response = await axios.post("/api/score-word", data = { gameId, word });
+  // if invalid move, display message
+
+  // if valid move, add word to playedwords
+  // $playedWords
+}
+
+$form.on("submit", checkWord)
 start();
